060f574ded2392bcebaea64622d2cfa2b08338eb
yaelBrown <yaelrbrown@gmail.com>
2020-03-24 01:42:35 -0500

    added more stuff

--- install-dev.sh
+++ install-dev.sh
@@ -1,1 +1,1 @@
-sudo apt-get install git nodejs npm python-pip python3-pip mysql-server openjdk-11-jre-headless
+sudo apt-get install git nodejs npm python-pip python3-pip mysql-server openjdk-11-jre-headless python-dev libmysqlclient-dev 
