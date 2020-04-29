f90e81cf89e29c6f2b60bd81603bef7ac0a3c7c2
yaelBrown <yaelrbrown@gmail.com>
2020-04-29 14:42:55 -0500

    Able to get process and attach to it in sudo

--- shell_sandbox/one.sh
+++ shell_sandbox/one.sh
@@ -1,17 +1,10 @@
+#!/bin/bash
+
 echo "ProgressBar: Getting PID of api"
-pid=$(pidof python3)
-temp="$(cut -d' ' -f1 << $pid)"
+pids=$(pidof python3)
+IFS=' ' 
+read -ra ADDR <<< $pids
+pid=$ADDR
 echo "ProgressBar: PID of api is $pid"
 echo "ProgressBar: Tracing output from api"
-# strace -p $pid -e write
-
-# Trying to parse string of pid of the python3 process
-# https://unix.stackexchange.com/questions/312280/split-string-by-delimiter-and-get-n-th-element
-
-# stdbuf -oL python3 genNums.py |
-#   while IFS= read -r line
-#   do
-#     echo "$line"
-#   done
-
-
+strace -p $pid -e write
