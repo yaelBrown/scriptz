d9e97e12e785883e411bd441a05ed8663c5fd217
yaelBrown <yaelrbrown@gmail.com>
2020-05-01 17:31:09 -0700

    refactored install scripts

--- install-yael.sh
+++ install-yael.sh
@@ -1,18 +1,8 @@
-apt-get update
-apt-get install vifm gnome-tweak-tool
 add-apt-repository ppa:mamarley/quassel
-apt-get update
-apt-get install quassel
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install code # or code-insiders
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt-get install git nodejs npm
-sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -22,16 +12,7 @@ sudo apt-get install \
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-3.1
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install aspnetcore-runtime-3.1
-sudo apt-get update
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-runtime-3.1
+apt-get update
+apt-get install -y vifm gnome-tweak-tool quassel apt-transport-https code git nodejs npm apt-transport-https dotnet-sdk-3.1 aspnetcore-runtime-3.1 dotnet-runtime-3.1 ./google-chrome-stable_current_amd64.deb
