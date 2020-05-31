2755466d8030787fd6140892ad3bdc2fd26faf01
Yael Brown <yaelrbrown@gmail.com>
2020-05-31 01:08:12 -0500

    Added mysql uninstaller for mac

---
+++ mysql-uninstall.sh
@@ -0,0 +1,11 @@
+# Uninstall mysql on mac os x
+sudo rm /usr/local/mysql
+sudo rm -rf /usr/local/mysql*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+sudo rm -rf /Library/PreferencePanes/MySQL*
+rm -rf ~/Library/PreferencePanes/MySQL*
+sudo rm -rf /Library/Receipts/mysql*
+sudo rm -rf /Library/Receipts/MySQL*
+sudo rm -rf /var/db/receipts/com.mysql.*
+sudo rm -rf /Library/StartupItems/MySQLCOM
+brew uninstall mysql
